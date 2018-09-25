299cfff1cbde2eb58c1d8fbaebd3da0a5b223917
Nicola3009 <polozov97@mail.ru>
2018-09-25 09:08:25 +0500

    Николай Полозов. task14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -5,9 +5,8 @@ public class Task14 {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String strValue = Integer.toString(value);
+        return Integer.parseInt(new StringBuilder(strValue).reverse().toString());
     }
 
     public static void main(String[] args) {
